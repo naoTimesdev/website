bba47dc29032b3e61399a05dee5d04e5266e66d8
noaione <noaione0809@gmail.com>
2021-04-04 17:12:47 +0700

    Readd invite link injection

---
+++ website/src/pages/invite.js
@@ -0,0 +1,20 @@
+import Layout from "@theme/Layout";
+import React from "react";
+
+export default class InviteLink extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount () {
+    const script = document.createElement("script");
+    const InviteLink = "https://discord.com/api/oauth2/authorize?client_id=558256913926848537&permissions=1556606198&scope=bot%20applications.commands";
+    script.async = true;
+    script.innerHTML = `window.location = "${InviteLink}";`;
+    document.body.appendChild(script);
+  };
+
+  render() {
+    return <Layout title="Invite" />
+  }
+}
