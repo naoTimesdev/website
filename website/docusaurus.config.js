1c1e39574108015f29e387fad637209216c09da2
noaione <noaione0809@gmail.com>
2021-06-26 13:57:50 +0700

    remove buttons js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,7 +6,6 @@ module.exports={
   "organizationName": "naoTimesdev",
   "projectName": "website",
   "scripts": [
-    "https://buttons.github.io/buttons.js",
     {
       src: "https://tr.n4o.xyz/js/plausible.js",
       async: true,
